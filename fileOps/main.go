4729d4dadac3f5ab849dec74244cd8a2008d778c
namiththayyil@gmail.com <namiththayyil@gmail.com>
2024-09-14 19:58:55 +0530

    first commit

---
+++ fileOps/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	/"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+
+
+func main(){
+	//var fileName []string 
+	// fileName := os.Args[1]
+	// fmt.Println(fileName[1])
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+	
+	io.Copy(os.Stdout, file)
+	
+
+}
